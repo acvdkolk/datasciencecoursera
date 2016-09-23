5a36c336c5e893a1678f3da48077d70ddd1c0d2d
Ad van der Kolk <acvdkolk@planet.nl>
2016-09-23 12:04:23 +0200

    Commit HelloWorld

--- ELS-R v03.r
+++
@@ -1,6 +0,0 @@
-ELS = readLines('http://www.wonencentraal.nl/home/Shared-Applications/ELS-Webassistent-Smartlet/ELS-Statistics/ELS-Query-Statistics.html')
-head(ELS)
-pattern = '*inschrijven*'
-datalines = grep(pattern,ELS[1:length(ELS)], value=TRUE)
-datalines
-
---
+++ HelloWorld.md
@@ -0,0 +1,6 @@
+## This is a markdown file
+### And this is a small list
+* Keep your data safe and sound
+* Be transparant
+* Always save your raw data
+ 
